b1ab97bfa39fbbce431c2d38ccb230e7386a57d7
Constantin Barbu <constantin.i.barbu@gmail.com>
2018-10-04 17:24:27 +0200

    delete button via state

--- src/componenets/Contacts.js
+++ src/componenets/Contacts.js
@@ -24,12 +24,26 @@ class Contacts extends Component {
       }
     ]
   };
+
+  deleteContact = id => {
+    const { contacts } = this.state;
+    const newContacts = contacts.filter(contact => contact.id !== id);
+
+    this.setState({
+      contacts: newContacts
+    });
+  };
+
   render() {
     const { contacts } = this.state;
     return (
       <React.Fragment>
         {contacts.map(contact => (
-          <Contact contact={contact} key={contact.id} />
+          <Contact
+            contact={contact}
+            key={contact.id}
+            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
+          />
         ))}
       </React.Fragment>
     );
