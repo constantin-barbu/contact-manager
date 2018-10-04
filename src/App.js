803d2745f2de7ea976801728d44df0da78c5ed0b
Constantin Barbu <constantin.i.barbu@gmail.com>
2018-10-04 17:10:13 +0200

    added bootstrap, contacts + contact, header, state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import Contacts from "./componenets/Contacts";
+import Header from "./componenets/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contacts />
+        </div>
       </div>
     );
   }
